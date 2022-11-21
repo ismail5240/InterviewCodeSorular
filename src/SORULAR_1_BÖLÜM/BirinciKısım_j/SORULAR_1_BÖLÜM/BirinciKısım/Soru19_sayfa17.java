e9189b3c9b10d8ad90d1d969d35e431a0359e95f
ismail5240 <arkd50279@gmail.com>
2022-11-21 20:36:35 +0300

    21/11/22

--- src/SORULAR_1_BÖLÜM/BirinciKısım_j/SORULAR_1_BÖLÜM/BirinciKısım/Soru19_sayfa17.java
+++ src/SORULAR_1_BÖLÜM/BirinciKısım_j/SORULAR_1_BÖLÜM/BirinciKısım/Soru19_sayfa17.java
@@ -8,8 +8,14 @@ public class Soru19_sayfa17 {
         int num=1234;
         int rev=0;
 
-        while (num!=0);{
-            int n=num%10;
+        while (num!=0){
+            int n=num%10;//n1(4)-->n2(3)-->n3(2)-->n4(1)
+            rev=rev*10+n;
+            System.out.println(rev);//4-->43-->432-->4321
+            num=num/10;
+            System.out.println(num);//num1(123)-->num2(12)-->num3(1)-->0
         }
+        //System.out.println(rev);//43
     }
+
 }
